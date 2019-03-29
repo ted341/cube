e3229c200f48a7863ecadd654224d105d87bcd01
hcchang0701 <ted341@gmail.com>
2019-03-29 16:31:21 +0800

    jQuery included

--- WebApp/scripts/cube.js
+++ WebApp/scripts/cube.js
@@ -1,14 +1,15 @@
-const log = new URL('https://script.google.com/macros/s/AKfycbxsvNzRf4sXFbgCZrovJNipy9rK3rYdcN5_o5PQJSrcA5DN985M/exec');
+// this is the testing script
+const log = new URL('https://script.google.com/macros/s/AKfycbz9gBHjjgySc9eLQuEQb0ig5LqLYbkcuRWE6eKG/exec');
 const bgname = ['darksalmon','tomato','orange','palegreen','dodgerblue','mediumseagreen','slateblue','violet']
-const bghex = ['#e9967a','#ff6347','#ffa500','#98fb98','#1e90ff','#3cb371','#6a5acd','#ee82ee']
+//const bghex = ['#e9967a','#ff6347','#ffa500','#98fb98','#1e90ff','#3cb371','#6a5acd','#ee82ee']
 const top3 = [0, 3, 8, 1, 2, 7, 4, 5, 6], top4 = [0, 3, 8, 15, 1, 2, 7, 14, 4, 5, 6, 13, 9, 10, 11, 12],
       top5 = [0, 3, 8, 15, 24, 1, 2, 7, 14, 23, 4, 5, 6, 13, 22, 9, 10, 11, 12, 21, 16, 17, 18, 19, 20];
 
-var col, toplist;
+var col, toplist, spl;
 function createBoard(){
 
   toplist = []; // buffer
-  arr = [];
+  arr = []; // list of odds
 
   col = Array(size*2);
   for (let i = 0; i < size; i++){
@@ -16,40 +17,37 @@ function createBoard(){
     col[i + size] = size*2;
   }
 
-  var t = document.getElementById("TopFace"),
-      l = document.getElementById("LeftFace"),
-      r = document.getElementById("RightFace");
-
   // create blank element of board
   for (let i = 0; i < size*2; i++){
     for (let j = 0; j < col[i]; j++){
 
-      let newel = document.createElement('div');
-
-      newel.id = i*size*2+j;
-      newel.className = "square";
-      newel.style.backgroundColor = 'white';
-      newel.style.height = newel.style.width = String(150/size) + "px";
-      newel.addEventListener('click', changeColor, false);
+      let newel = $("<div></div>");
+      newel.attr("id", i*size*2+j);
+      newel.attr("class", "square");
+      newel.css("background-color", "white");
+      newel.css("height", String(150/size));
+      newel.css("width", String(150/size));
+      newel.on("click", changeColor);
 
       if (i < size) toplist.push(newel);
-      else if (j < size) l.appendChild(newel);
-      else r.appendChild(newel);
+      else if (j < size) $("#LeftFace").append(newel);
+      else $("#RightFace").append(newel);
     }
   }
 
   for (let i = 0; i < toplist.length; i++)
     switch (size){ // raarrange top face element
-      case 3: t.appendChild(toplist[top3[i]]); break;
-      case 4: t.appendChild(toplist[top4[i]]); break;
-      case 5: t.appendChild(toplist[top5[i]]); break;
+      case 3: $("#TopFace").append(toplist[top3[i]]); break;
+      case 4: $("#TopFace").append(toplist[top4[i]]); break;
+      case 5: $("#TopFace").append(toplist[top5[i]]); break;
     }
 
   fetchBoard().then(q => {
     console.log(q);
-    pass  = Array(cnum).fill(false);
     spath = new Array(cnum); // shortest path container
+    pass = Array(cnum).fill(0);
     for (let i = 0; i < cnum; i++) spath[i] = [];
+    spl = q.spl;
   })
 
   setBoardStyle();
@@ -57,38 +55,28 @@ function createBoard(){
 }
 
 function destroyBoard(){
-
   if (size === undefined) return;
-
-  var top = document.getElementById("TopFace"),
-      left = document.getElementById("LeftFace"),
-      right = document.getElementById("RightFace");
-
-  var topElement = document.querySelectorAll('#TopFace .square'),
-      leftElement = document.querySelectorAll('#LeftFace .square'),
-      rightElement = document.querySelectorAll('#RightFace .square');
-
-  for (let i = 0; i < size**2; i++){
-    top.removeChild(topElement[i]);
-    left.removeChild(leftElement[i]);
-    right.removeChild(rightElement[i]);
-  }
+  $("#cube>div").removeAttr("style");
+  $("#cube>div").empty();
 }
 
 var complete;
 function resetBoard(){
 
-  if (complete !== undefined && complete) return;
+  if (complete === undefined || complete) return;
 
-  var el = document.getElementsByClassName("square");
-  for (let i = 0; i < el.length; i++){
-    let idx = arr.indexOf(parseInt(el[i].id));
-    if (idx > -1) el[i].style.backgroundColor = bgname[Math.floor(idx/2)];
-    else el[i].style.backgroundColor = "white";
-    el[i].style.borderColor = "black";
+  var sq = $(".square");
+
+  for (s in sq){ // s is index of jquery object (string)
+    let idx = arr.indexOf(parseInt(sq.eq(s).attr("id")));
+    if (idx > -1) sq.eq(s).css("background-color", bgname[Math.floor(idx/2)]);
+    else sq.eq(s).css("background-color", "white");
+    sq.eq(s).css("border-color", "black");
+    if (parseInt(s)+1 == sq.length) break;
   }
 }
 
+// unused
 function generateBoard(){
   // randomly pick odds
   while(arr.length < cnum*2){
@@ -103,12 +91,15 @@ function generateBoard(){
   }
 }
 
-var cnum, arr; // arr: list of odds
+var board, cnum, arr; // arr: list of odds
 async function fetchBoard(){
 
   try {
-    var res = await fetch('data/' + String(size) + '.json');
-    var board = await res.json();
+    if (board === undefined || board[0].size != size){
+      var res = await fetch('data/' + String(size) + '.json');
+      if (!res.ok) throw new Error(res.statusText)
+      else board = await res.json();
+    }
   } catch(e){ return Promise.reject(e); }
 
   var index = Math.floor(Math.random()*Object.keys(board).length);
@@ -125,58 +116,40 @@ async function fetchBoard(){
 }
 
 function setOddStyle(id){
-  let img = 'url("data/frame.png")';
+
   let e = document.getElementById(id).style;
-  e.backgroundImage = img;
-  e.backgroundPosition = "center";
+  e.backgroundImage = 'url("data/frame.png")';
+  e.backgroundPosition = "top 50% left 50%";
   e.backgroundRepeat = "no-repeat";
   e.backgroundColor = bgname[Math.floor((arr.length-1)/2)];
-
-  let len = String(120/size) + "px";
-  e.backgroundSize = len + " " + len;
+  e.backgroundSize = "80%";
+  //String(120/size) + " " + String(120/size);
 }
 
 function setBoardStyle(){
 
   var t = document.getElementById("TopFace").style,
       l = document.getElementById("LeftFace").style,
-      r = document.getElementById("RightFace").style;
-  var trans = "rotate(90deg) translate(60px,-168px) \
-               skew(30deg,-60deg) scale(0.58, 1)";
+      r = document.getElementById("RightFace").style,
+      length;
 
   switch(size){
-    case 3:
-      t.height = t.width = '162px';
-      r.height = r.width = '162px';
-      l.height = l.width = '162px';
-
-      t.transform = trans;
-      r.transform = "translate(-89px,26px)";
-      l.transform = "translate(87px,26px)";
-      break;
-    case 4:
-      t.height = t.width = '168px';
-      r.height = r.width = '168px';
-      l.height = l.width = '168px';
-
-      t.transform = trans;
-      r.transform = "translate(-79px, 22px)";
-      l.transform = "translate(87px, 22px)";
-      break;
-    case 5:
-      t.height = t.width = '170px';
-      r.height = r.width = '170px';
-      l.height = l.width = '170px';
-
-      t.transform = trans;
-      r.transform = "translate(-77px,24px)";
-      l.transform = "translate(83px,24px)";
-      break;
+    case 3: length = '162px'; break;
+    case 4: length = '166px'; break;
+    case 5: length = '170px'; break;
   }
 
-  r.transform += "skew(0deg,-30deg)";
-  l.transform += "skew(0deg,30deg)";
+  t.height = t.width = r.height = r.width = l.height = l.width = length;
+  t.transform = "skew(60deg, -30deg) scale(1, 0.577) rotate(90deg)";
+  r.transform = "skew(0deg, -30deg)";
+  l.transform = "skew(0deg, 30deg)";
+
+  if (size == 3){ t.left = '33.8%'; t.top = '11.8%'; }
+  else{ t.left = '33.1%'; t.top = '12.2%'; }
 }
+
+// unused
+function setButtonStyle(){}
 
 var edge;
 function constructEdgeList(){
@@ -205,7 +178,7 @@ function constructEdgeList(){
   }
 }
 
-var curColor;
+var curColor, moves, steps;
 function changeColor(){
 
   var clkColor = this.style.backgroundColor;
@@ -216,39 +189,50 @@ function changeColor(){
     else curColor = clkColor;
   }
   else if (curColor != clkColor && curColor != 'white'){
+
     this.style.backgroundColor = curColor;
+    moves.push([parseInt(this.id), ticks]); // (pos, time)
+    steps++;
+
     if (checkConnection()){
-      document.getElementById("pass").innerHTML = "Complete!";
+      let msg = $("<p></p>")
+      msg.html("Complete!");
+      $("#pass").append(msg);
+      $("#pass").fadeIn(1000);
       complete = true;
     }
   }
 }
 
-var size;
+var size, start;
 function changeSize(){
   // entrance of the web app
   destroyBoard();
 
-  // resetting
+  // reset/initialize
+  start = new Date();
   size = parseInt(this.id[0]);
   curColor = "white";
   complete = false;
-  document.getElementById("pass").innerHTML = "";
+  ticks = 0;
+  steps = 0;
+  moves = [];
+  $('#pass').hide(0);
+  $('#time').show(0);
+  $('#intro').hide(0);
 
   createBoard();
 
-  if (timerId !== undefined){
-    clearInterval(timerId);
-    document.getElementById("time").innerHTML = "Time: 00:00:00";
-  }
+  if (complete == false) clearInterval(timerId);
   timer();
 }
 
-var nblocks, pass, spath;
+var nblocks, spath, pass, ans;
 function checkConnection(){
 
-  var finish = true;
+  var finish = true, count = 0;
   nblocks = 3*size**2;
+  ans = 0;
 
   for (let j = 0; j < cnum; j++){
 
@@ -262,12 +246,15 @@ function checkConnection(){
     dis[arr[j*2]] = 0;
     vis[arr[j*2]] = true;
 
+    while (spath[j].length)
+      document.getElementById(spath[j].pop()).style.borderColor = "black";
+
     while(que.length > 0 && !flag){
 
       let tmp = que.shift();
       for (let i = 0; i < edge[tmp].length; i++){
 
-        let next = edge[tmp][i],
+        let next = edge[tmp][i], // cannot use jquery
             color = document.getElementById(next).style.backgroundColor;
 
         if (!vis[next] && color == bgname[j])
@@ -279,11 +266,12 @@ function checkConnection(){
 
           if (next == arr[j*2+1])
           {
-            let p = next;
-            while(pre[p] != arr[j*2])
+            ans++;
+            while(pre[next] != arr[j*2])
             {
-              spath[j].push(pre[p]);
-              p = pre[p];
+              spath[j].push(pre[next]);
+              next = pre[next];
+              ans++;
             }
             flag = true;
             break;
@@ -292,23 +280,20 @@ function checkConnection(){
       }
     }
 
-    if (flag) // the color is connected
-    {
-      pass[j] = true;
+    if (flag){ // the color is connected
       for (let k = 0; k < spath[j].length; k++)
         document.getElementById(spath[j][k]).style.borderColor = "white";
+      if(pass[count] == 0)
+        pass[count] = ticks;
+      count++;
     }
-    else
-    {
-      pass[j] = finish = false;
-      while (spath[j].length)
-        document.getElementById(spath[j].pop()).style.borderColor = "black";
-    }
+    else finish = false;
   }
 
   if (finish){
     // stop timer when all color is connected
     clearInterval(timerId);
+    //console.log(pass);
     postRecord();
     return true;
   }
@@ -317,38 +302,36 @@ function checkConnection(){
 
 function postRecord(){
   ///*
-  let record = { duration: time, size: size, color: cnum }
-  let form = new FormData();
-  form.append('duration', time);
-  form.append('size', size);
-  form.append('color', cnum);
-  //let params = {name: person, duration: time, size: size, color: cnum};
-  //log.search = new URLSearchParams(params);
-  /*
-  fetch(log, { method: 'options', headers: {'Access-Control-Request-Method': 'post', 'Access-Control-Request-Headers': 'Content-Type'}})
-  .then(preflight => {
-    if (!preflight.ok) throw new Error(preflight.statusText)
+  var form = new FormData();
+  form.append('name', name); // name of player
+  form.append('spent', time); // total time spent (regular format)
+  form.append('size', size); // board size
+  form.append('color', cnum); // number of colors
+  form.append('pairs', arr); // pairs' id
+  form.append('moves', moves); // position $ time in every move
+  form.append('ticks', ticks); // total time spent in second(s)
+  form.append('steps', steps); // total click times
+  form.append('checkpoints', pass); // time spent in connecting numbers of pairs
+  form.append('answer', ans); // real total path length
+  form.append('spl', spl);
 
-  })
-  //*/
-  fetch(log, { method: 'post', body: JSON.stringify(record), mode: 'cors', credentials: 'include', headers: {'Content-Type': 'application/json'}})
+  fetch(log, { method:'post', body:form, mode:'cors' })
   .then(response => {
     if (!response.ok) throw new Error(response.statusText)
     return response.text()
-  }).catch(err => console.log(err))
-  .then(t => console.log(t))
+  }).then(t => console.log(t))
+  .catch(err => console.log(err))
   //*/
 }
 
-var time, timerId;
+var time, timerId, ticks;
 function timer(){
-
-  timerId = setInterval(stringProcessor, 1000);
-
+  //timerId = setInterval(stringProcessor, 1000);
+  /*
   function stringProcessor(){
 
-    var e = document.getElementById("time");
-    time = e.innerHTML.replace(/[^0-9]/g, "");
+    var e = $("#time");
+    time = e.html().replace(/[^0-9]/g, "");
 
     var carry = false, // default radix is 10
         hh = parseInt(time.substr(0, 2)),
@@ -372,12 +355,27 @@ function timer(){
     hh = ("00" + hh).substr(hh.length);
 
     time = "Time: " + hh + ":" + mm + ":" + ss;
-    e.innerHTML = time;
+    e.html(time);
+    ticks++;
+  }
+  */
+  timerId = setInterval(dateDiff, 1);
+
+  function dateDiff(){
+
+    var dt = Date.now() - start.getTime(),
+        diff = new Date(dt);
+
+    time = diff.toISOString().substr(11, 12);
+    $('#time>p').html(time);
+    ticks = diff.getTime()/1000;
   }
 }
 
-var button = document.getElementsByClassName("boardsize");
-for (let i = 0; i < button.length; i++)
-  button[i].addEventListener('click', changeSize, false);
-
-document.getElementById("clear").onclick = resetBoard;
+var name;
+$(function(){
+  name = prompt("Please enter your name", "anonym");
+  $('.Boardsize').on("click", changeSize); // must use "on"
+  $('#Clear').click(resetBoard);
+  $('#time').hide(0);
+})
