9743034e7b01b1561228cd25e91d28ac338a787f
Ted <ted341@fmail.com>
2019-09-20 22:23:44 +0800

    update

---
+++ web_app/scripts/cube_bv.js
@@ -0,0 +1,13 @@
+Vue.component('b-blk', {
+    data: function() {
+        return {
+            color: "white"
+        }
+    },
+    template: '<div></div>'
+})
+
+var vm = new Vue({
+    el: '#app'
+})
+ 
